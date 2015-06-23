dc14dbb0d7cb575726b015401e23dec8017cd132
Thomas Leskin <tleskin@gmail.com>
2015-06-23 09:58:41 -0600

    Wrapped test in VCR and set it to create cassettes in a sunlight_service_legislators.yml file

--- test/services/sunlight_service_test.rb
+++ test/services/sunlight_service_test.rb
@@ -2,11 +2,13 @@ require 'test_helper'
 
 class SunlightServiceTest < ActiveSupport::TestCase
   test 'it gets all legislators by location' do
-    service = SunlightService.new
-    results = service.legislators('80202')
+    VCR.use_cassette('sunlight_service#legislators') do
+      service = SunlightService.new
+      results = service.legislators('80202')
 
-    assert_equal 3, results.count
-    assert_equal 'Cory', results.first[:first_name]
-    assert_equal 'Gardner', results.first[:last_name]
+      assert_equal 3, results.count
+      assert_equal 'Cory', results.first[:first_name]
+      assert_equal 'Gardner', results.first[:last_name]
+    end
   end
 end
