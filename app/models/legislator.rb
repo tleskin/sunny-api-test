670ace84ed9f350cc59b39c44d42c55641cb341d
Thomas Leskin <tleskin@gmail.com>
2015-06-23 10:25:48 -0600

    Change legislator class to inherit from OpenStruct so can access the hash as an object

--- app/models/legislator.rb
+++ app/models/legislator.rb
@@ -1,9 +1,11 @@
-class Legislator
+class Legislator < OpenStruct
   def self.service
     @service ||= SunlightService.new
   end
 
   def self.find_by(zipcode: "")
-    service.legislators(zipcode)
+    service.legislators(zipcode).map do |legislator|
+      Legislator.new(legislator)
+    end
   end
 end
